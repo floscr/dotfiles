d0ec2dd9752ebeaafc458d62be18eb5e29dfd874
Florian Schroedl <flo.schroedl@gmail.com>
2020-12-30 17:10:26 +0100

    More copy

--- flake.nix
+++ flake.nix
@@ -1,12 +1,26 @@
+# flake.nix --- the heart of my dotfiles
+#
+# Author:  Henrik Lissner <henrik@lissner.net>
+# URL:     https://github.com/hlissner/dotfiles
+# License: MIT
+#
+# Welcome to ground zero. Where the whole flake gets set up and all its modules
+# are loaded.
+
 {
+  description = "A grossly incandescent nixos config.";
+
   inputs =
     {
+      # Core dependencies.
+      # Two inputs so I can track them separately at different rates.
       nixpkgs.url          = "nixpkgs/master";
       nixpkgs-unstable.url = "nixpkgs/master";
 
       home-manager.url   = "github:rycee/home-manager/master";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+      # Extras
       emacs-overlay.url  = "github:nix-community/emacs-overlay";
       nixos-hardware.url = "github:nixos/nixos-hardware";
     };
@@ -20,15 +34,25 @@
 
       mkPkgs = pkgs: extraOverlays: import pkgs {
         inherit system;
-        config.allowUnfree = true;
+        config.allowUnfree = true;  # forgive me Stallman senpai
+        overlays = extraOverlays ++ (attrValues self.overlays);
       };
-      pkgs = mkPkgs nixpkgs [];
+      pkgs  = mkPkgs nixpkgs [ self.overlay ];
       uPkgs = mkPkgs nixpkgs-unstable [];
 
       lib = nixpkgs.lib.extend
         (self: super: { my = import ./lib { inherit pkgs inputs; lib = self; }; });
     in {
       lib = lib.my;
+
+      overlay =
+        final: prev: {
+          unstable = uPkgs;
+          my = self.packages."${system}";
+        };
+
+      overlays =
+        mapModules ./overlays import;
 
       packages."${system}" =
         mapModules ./packages
@@ -40,5 +64,5 @@
 
       nixosConfigurations =
         mapHosts ./hosts { inherit system; };
-      };
+    };
 }
