664ab29c079932f26cd0d1bcd2c494e5b5c131c6
Florian Schroedl <flo.schroedl@gmail.com>
2020-12-30 14:47:20 +0100

    Remove homeManager

--- default.nix
+++ default.nix
@@ -1,10 +1,9 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-with inputs;
 {
-  imports =
-    [ home-manager.nixosModules.home-manager ];
+  # imports =
+  #   [ home-manager.nixosModules.home-manager ];
 
   # Configure nix and nixpkgs
   environment.variables.NIXPKGS_ALLOW_UNFREE = "1";
