8f1c775739caa317576f08aa6a2d25ec4ae1a301
Florian Schroedl <flo.schroedl@gmail.com>
2020-12-30 13:56:56 +0100

    Base setup

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs =
+    {
+      nixpkgs.url          = "nixpkgs/master";
+      nixpkgs-unstable.url = "nixpkgs/master";
+
+      home-manager.url   = "github:rycee/home-manager/master";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+      emacs-overlay.url  = "github:nix-community/emacs-overlay";
+      nixos-hardware.url = "github:nixos/nixos-hardware";
+    };
+  outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs;
+      uPkgs = nixpkgs-unstable;
+    in {
+      nixosConfigurations = {
+        macbook-air = {
+          inherit pkgs system;
+          modules = [
+            ./hosts/macbook-air/configuration.nix
+          ];
+        };
+      };
+    };
+}
