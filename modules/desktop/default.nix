08d5d7485a0e1381e46b3bdd082f890879e9f4d2
Florian Schroedl <flo.schroedl@gmail.com>
2020-12-31 16:20:05 +0100

    Add desktop

---
+++ modules/desktop/default.nix
@@ -0,0 +1,67 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop;
+in {
+  config = mkIf config.services.xserver.enable {
+    assertions = [
+      {
+        assertion = (countAttrs (n: v: n == "enable" && value) cfg) < 2;
+        message = "Can't have more than one desktop environment enabled at a time";
+      }
+      {
+        assertion =
+          let srv = config.services;
+          in srv.xserver.enable ||
+             srv.sway.enable ||
+             !(anyAttrs
+               (n: v: isAttrs v &&
+                      anyAttrs (n: v: isAttrs v && v.enable))
+               cfg);
+        message = "Can't enable a desktop app without a desktop environment";
+      }
+    ];
+
+    user.packages = with pkgs; [
+      xclip
+      xdotool
+    ];
+
+    fonts = {
+      fontDir.enable = true;
+      enableGhostscriptFonts = true;
+      fonts = with pkgs; [
+        dejavu_fonts
+        fira-code
+        fira-code-symbols
+        ubuntu_font_family
+        ibm-plex
+
+        # Unicode and Symbols
+        font-awesome-ttf
+        noto-fonts
+        noto-fonts-cjk
+        symbola
+      ];
+    };
+
+    # Try really hard to get QT to respect my GTK theme.
+    env.GTK_DATA_PREFIX = [ "${config.system.path}" ];
+    env.QT_QPA_PLATFORMTHEME = "gtk2";
+    qt5 = { style = "gtk2"; platformTheme = "gtk2"; };
+
+    services.xserver.displayManager.sessionCommands = ''
+      # GTK2_RC_FILES must be available to the display manager.
+      export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+    '';
+
+    # Clean up leftovers, as much as we can
+    system.userActivationScripts.cleanupHome = ''
+      pushd "${homeDir}"
+      rm -rf .compose-cache .nv .pki .dbus .fehbg
+      [ -s .xsession-errors ] || rm -f .xsession-errors*
+      popd
+    '';
+  };
+}
