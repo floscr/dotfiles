c4d29d0988dd32aa6e214f8025a54d9c8231b169
Florian Schroedl <flo.schroedl@gmail.com>
2020-12-31 11:14:28 +0100

    Switch to plasma

--- hosts/mbair/default.nix
+++ hosts/mbair/default.nix
@@ -17,10 +17,23 @@ with lib.my;
   };
 
   networking.useDHCP = false;
+  services.xserver = {
+    enable = true;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+    windowManager.bspwm.enable = true;
 
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
+    # Configure keymap in X11
+    layout = "us";
+    xkbOptions = "eurosign:e";
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    libinput.enable = true;
+  };
+
+  # services.xserver.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome3.enable = true;
   
   environment.systemPackages = with pkgs; [
     firefox
