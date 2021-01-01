35f7d40786baf5d0763cc6a3e41c37b07ca04602
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-01 17:39:45 +0000

    Add bindings

---
+++ modules/bindings.nix
@@ -0,0 +1,31 @@
+{ config, options, lib, home-manager, ... }:
+
+with lib;
+with lib.my;
+{
+  options = with types; {
+    bindings = mkOption {
+      type = with types; listOf (submodule({ name, ... }: {
+        options.binding = mkOption { type = nullOr str; default = null; };
+        options.command = mkOption { type = str; default = ""; };
+        options.description = mkOption { type = str; default = ""; };
+        options.categories = mkOption { type = str; default = ""; };
+      }));
+      default = [];
+    };
+  };
+
+  config = {
+    # Rofi CMDer
+    home.configFile."cmder/cmd.csv"=
+      fold (cur: acc: acc + "${cur.description},,,${cur.command},,,${if ! isNull(cur.binding) then cur.binding else ""}\n") "" config.my.bindings;
+    # Sxhkd bindings
+    home.configFile."sxhkd/sxhkdrc" =
+      fold (cur: acc: if isNull cur.binding then acc else ''
+${acc}
+# ${cur.description}
+${cur.binding}
+    ${cur.command}
+'') "" config.my.bindings;
+  };
+}
