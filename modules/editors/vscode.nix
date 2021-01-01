5d93f9711c966fc7018c119732080590f7deefc0
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-01 16:27:24 +0100

    Add vscode modules

---
+++ modules/editors/vscode.nix
@@ -0,0 +1,16 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.editors.vscode;
+in {
+  options.modules.editors.vscode = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      vscodium
+    ];
+  };
+}
