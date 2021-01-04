c4c9b364e4007c8b37542da79049e1ed0c7c8777
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-04 09:50:16 +0100

    Add colors

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -20,6 +20,37 @@ in {
       '';
     };
 
+    colors = mkOption {
+      type = with types; nullOr submodule({ name, ... }: {
+        options.black1 = str;
+        options.black2 = str;
+        options.white = str;
+        options.grey1 = str;
+        options.grey1blue = str;
+        options.grey2 = str;
+        options.red = str;
+        options.bred = str;
+        options.grn = str;
+        options.bgrn = str;
+        options.yellow = str;
+        options.byellow = str;
+        options.blue = str;
+        options.bblue = str;
+        options.mag = str;
+        options.bmag = str;
+        options.cyn = str;
+        options.bcyn = str;
+
+        # Aliases
+        options.terminalBackground = str;
+        options.background = str;
+        options.text = str;
+        options.fail = str;
+        options.success = str;
+      });
+      default = null;
+    };
+
     onReload = mkOpt (attrsOf lines) {};
   };
 
