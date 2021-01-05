58fa4ed54fd5bff308b2773ae98a4d4fb4019184
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-05 17:48:08 +0100

    Cleanup

---
+++ modules/dev/nim.nix
@@ -0,0 +1,35 @@
+{ config, options, lib, pkgs, my, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.nim;
+in {
+  options.modules.dev.nim = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      nim
+      (pkgs.writeScriptBin "nimx" ''
+        bin=$1
+        src="$DOTFILES/nimbin/src/$bin.nim"
+        dst="$DOTFILES/nimbin/dst/$bin"
+        shift
+
+        if [[ ! -f "$dst" || "$src" -nt "$dst" ]]; then
+            echo "Compiling $src..."
+            ${pkgs.nim}/bin/nim c -r \
+                --verbosity:0 \
+                --hint[Processing]:off \
+                --excessiveStackTrace:on \
+                -d:release \
+                --out:$dst \
+                $src
+        fi
+
+        $dst $@
+      '')
+    ];
+  };
+}
