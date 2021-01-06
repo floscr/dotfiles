1d74fe7c59a7ad33535e5501433df0d25d3c7780
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-06 15:22:42 +0100

    Add scanner

---
+++ modules/hardware/scanner.nix
@@ -0,0 +1,23 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.hardware.scanner;
+in {
+  options.modules.hardware.scanner = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      scantailor
+    ];
+
+    hardware.sane.enable = true;
+    hardware.sane.extraBackends = with pkgs; [
+      epkowa
+      utsushi
+    ];
+    services.udev.packages = [ pkgs.utsushi ];
+  };
+}
