bf37b4c3d767b8898f323cb2747915e9ca9ad8cb
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-06 15:16:44 +0100

    Add redshift

---
+++ modules/services/redshift.nix
@@ -0,0 +1,33 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.services.redshift;
+in {
+  options.modules.services.redshift = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.redshift = {
+      enable = true;
+      temperature = {
+        day = 5500;
+        night = 3000;
+      };
+    };
+
+    location = {
+      longitude = 12.5;
+      latitude = 55.88;
+    };
+
+    modules.bindings.items = [
+      {
+        description = "Toggle Redshift";
+        categories = "Script";
+        command = "toggle_redshift";
+      }
+    ];
+  };
+}
