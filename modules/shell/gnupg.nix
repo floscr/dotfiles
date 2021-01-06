6e0ae5058975959b59203417683d66304c2a86a2
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-06 15:28:59 +0100

    Cleanup

--- modules/shell/gnupg.nix
+++ modules/shell/gnupg.nix
@@ -14,7 +14,7 @@ in {
 
     programs.gnupg.agent.enable = true;
 
-    user.packages = [ pkgs.tomb ];
+    user.packages = with pkgs; [ tomb ];
 
     # HACK Without this config file you get "No pinentry program" on 20.03.
     #      programs.gnupg.agent.pinentryFlavor doesn't appear to work, and this
