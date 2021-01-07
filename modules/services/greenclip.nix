efe5656a07ef02c11d77af5a9431997c895e5d77
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-07 14:30:29 +0100

    Fix greenclip

--- modules/services/greenclip.nix
+++ modules/services/greenclip.nix
@@ -8,44 +8,46 @@ in {
     enable = mkBoolOpt false;
   };
 
-  config = mkIf cfg.enable {
-    user.packages = with pkgs; [
-      greenclip
-      pkgs.xorg.libXdmcp
-      (let rofi = "${pkgs.rofi}/bin/rofi";
-            xdotool = "${pkgs.xdotool}/bin/xdotool";
-            xclip = "${pkgs.xclip}/bin/xclip";
-        in pkgs.writeShellScriptBin "rofi-greenclip" ''
-          #!${pkgs.stdenv.shell}
-          ${rofi} -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
-          if [[ "$1" == "type" ]]; then
-              sleep 0.5
-              ${xdotool} type "$(${xclip} -o -selection clipboard)"
-          fi
+  config = mkIf cfg.enable (
+    let greenclip = pkgs.haskellPackages.greenclip;
+    in {
+      user.packages = with pkgs; [
+        greenclip
+        xorg.libXdmcp
+        (let rofi = "${pkgs.rofi}/bin/rofi";
+             xdotool = "${pkgs.xdotool}/bin/xdotool";
+             xclip = "${pkgs.xclip}/bin/xclip";
+         in pkgs.writeShellScriptBin "rofi-greenclip" ''
+            #!${pkgs.stdenv.shell}
+            ${rofi} -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}'
+            if [[ "$1" == "type" ]]; then
+                sleep 0.5
+                ${xdotool} type "$(${xclip} -o -selection clipboard)"
+            fi
       '')
-    ];
-    modules.bindings.items = [
-      {
-        binding = "super + shift + v";
-        command = "rofi-greenclip";
-        description = "Greenclip";
-        categories = "Clipboard Manager";
-      }
-      {
-        binding = "super + alt + shift + v";
-        command = "rofi-greenclip type";
-        description = "Greenclip (Type selection)";
-        categories = "Clipboard Manager";
-      }
-    ];
-    systemd.user.services.greenclip = {
-      enable= true;
-      description = "greenclip daemon";
-      wantedBy = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        ExecStart = "${greenclip}/bin/greenclip daemon";
+      ];
+      modules.bindings.items = [
+        {
+          binding = "super + shift + v";
+          command = "rofi-greenclip";
+          description = "Greenclip";
+          categories = "Clipboard Manager";
+        }
+        {
+          binding = "super + alt + shift + v";
+          command = "rofi-greenclip type";
+          description = "Greenclip (Type selection)";
+          categories = "Clipboard Manager";
+        }
+      ];
+      systemd.user.services.greenclip = {
+        enable= true;
+        description = "greenclip daemon";
+        wantedBy = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+          ExecStart = "${greenclip}/bin/greenclip daemon";
+        };
       };
-    };
-  };
+    });
 }
