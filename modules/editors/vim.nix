f46b8c0d361dffbd6ddbc3d32290429854c99208
Florian Schroedl <flo.schroedl@gmail.com>
2021-01-07 20:51:26 +0100

    Configure vim

--- modules/editors/vim.nix
+++ modules/editors/vim.nix
@@ -14,9 +14,31 @@ in {
       neovim
     ];
 
-    environment.shellAliases = {
-      vim = "nvim";
-      v   = "nvim";
+    programs.neovim = {
+      enable = true;
+      vimAlias = true;
+      viAlias = true;
+
+      configure = {
+        packages.metalogical = with pkgs.vimPlugins; {
+          start = [
+            vim-airline
+            fzfWrapper fzf-vim
+            deoplete-nvim LanguageClient-neovim
+            vim-nix # vim-addon-nix?
+            vim-go # TODO move to go-dev.nix
+          ];
+          opt = [ ];
+        };
+        customRC = ''
+          set undodir=$XDG_DATA_HOME/vim/undo
+          set directory=$XDG_DATA_HOME/vim/swap
+          set backupdir=$XDG_DATA_HOME/vim/backup
+          set viewdir=$XDG_DATA_HOME/vim/view
+          set viminfo+='1000,n$XDG_DATA_HOME/vim/viminfo
+          set runtimepath=$XDG_CONFIG_HOME/vim,$VIMRUNTIME,$XDG_CONFIG_HOME/vim/after
+        '';
+      };
     };
   };
 }
