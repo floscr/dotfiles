26328b67b7ee2ebcbc2cd0ed4e259a2145e7f580
Florian Schroedl <flo.schroedl@gmail.com>
2021-03-10 22:39:04 +0100

    Nmtui parsing

--- bin/nimbin/src/wifi.nim
+++ bin/nimbin/src/wifi.nim
@@ -1,16 +1,70 @@
-import system
-import lib/utils
-import fp/option
-import fp/list
 import sugar
+import system
+import strutils
+import sequtils
+# import unpack
+import fp/either
+import fp/list
+import fp/option
+import lib/utils
+import lib/shUtils
+import lib/fpUtils
 
-proc wifiSsid(): Option[string] =
-  shellCommand("nmcli con show --active")
-    .grep("wifi")
-    .asList
-    .headOption
-    .notEmpty
-    .map(x => x.getColumn(0))
-    .notEmpty
+{.experimental.}
 
-echo wifiSsid().getOrElse("")
+type
+  ConfigItem = ref object
+    name: string
+    uuid: string
+    typeId: string
+    device: string
+
+proc extractNetworkData(xs: seq[string]): Option[ConfigItem] =
+  xs
+    .some
+    .filter(xs => xs.len == 4)
+    .map(x => ConfigItem(
+      name: x[0],
+      uuid: x[1],
+      typeId: x[2],
+      device: x[3]
+    ))
+
+proc wifiSsid(): any =
+  sh("nmcli con show --active")
+  .flatMap((x: string) => x
+       .split("\n")
+       .map((x: string) => x
+            .rsplit(" ")
+            .filter(x => x != "")
+            .some
+            .flatMap((x: seq[string]) => extractNetworkData(x))
+       )
+       .traverse((x: Option[ConfigItem]) => x)
+       .asEither("Could not parse \n\n" & x)
+  )
+  .flatMap((xs: seq[ConfigItem]) => xs
+           .asList
+           # .map((x: ConfigItem) => x.typeId)
+           .findX((x: ConfigItem) => x.typeId == "wifi")
+           .asEither("No wifi row found")
+  )
+
+    # .grep("wifi")
+    # .asList
+    # .headOption
+    # .notEmpty
+    # .map(x => x.getColumn(0))
+    # .notEmpty
+
+echo wifiSsid()
+.fold(
+  x => "Error:\n" & x,
+  (x: ConfigItem) => x.name,
+)
+
+
+# echo @[1, 2]
+#     .asList.findMe((x: int) => false)
+
+# echo @[1.some, 2.some, 3.some].traverse((x: Option[int]) => x)
