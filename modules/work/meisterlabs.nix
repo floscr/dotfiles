667d052256c45da8ed601aaec90866feb1d9cb90
Florian Schroedl <flo.schroedl@gmail.com>
2021-07-23 10:16:31 +0200

    Structure config

--- modules/work/meisterlabs.nix
+++ modules/work/meisterlabs.nix
@@ -9,36 +9,39 @@ in
     enable = mkBoolOpt false;
   };
 
-  config = mkIf cfg.enable {
-    user.packages = with pkgs; [
-      ruby
-      dragon-drop
-      (writeShellScriptBin "mm-build-bundle" ''
-        #!/usr/bin/env zsh
 
-        rm -rf build
-        nice -n10 npm run "$1"
-        notify-send "Bundle \"$1\" built."
-        $MEISTERLABS_MINDMEISTER_DOCKER_DIR/mindmeister/bundle
-        docker exec -it mm-rails /bin/bash -c "rm -rf /opt/mindmeister-web && mkdir -p /opt/mindmeister-web"
-        docker cp build mm-rails:/opt/mindmeister-web
-        cd $MEISTERLABS_MINDMEISTER_DOCKER_DIR
-        ./mindmeister/rake "client:import_bundles_testing['$2']"
-      '')
-    ];
+    # Default Config
+    {
+      user.packages = with pkgs; [
+        ruby
+        dragon-drop
+        (writeShellScriptBin "mm-build-bundle" ''
+          #!/usr/bin/env zsh
 
-    modules.shell.zsh.rcFiles = [ ./env.zsh ];
+          rm -rf build
+          nice -n10 npm run "$1"
+          notify-send "Bundle \"$1\" built."
+          $MEISTERLABS_MINDMEISTER_DOCKER_DIR/mindmeister/bundle
+          docker exec -it mm-rails /bin/bash -c "rm -rf /opt/mindmeister-web && mkdir -p /opt/mindmeister-web"
+          docker cp build mm-rails:/opt/mindmeister-web
+          cd $MEISTERLABS_MINDMEISTER_DOCKER_DIR
+          ./mindmeister/rake "client:import_bundles_testing['$2']"
+        '')
+      ];
 
-    modules.bindings.items = [
-      {
-        description = "MM: Start";
-        categories = "Work";
-        command = ''cd ~/Code/Meisterlabs/docker-dev-environment; ./mindmeister/restart; notify-send "MM Docker Started"'';
-      }
-      {
-        command = "${pkgs.dragon-drop}/bin/dragon --and-exit ~/Code/Meisterlabs/test-data/**/*.*";
-        description = "Dragon: Work DnD Test Data";
-      }
-    ];
-  };
-}
+      modules.shell.zsh.rcFiles = [ ./env.zsh ];
+
+      modules.bindings.items = [
+        {
+          description = "MM: Start";
+          categories = "Work";
+          command = ''cd ~/Code/Meisterlabs/docker-dev-environment; ./mindmeister/restart; notify-send "MM Docker Started"'';
+        }
+        {
+          command = "${pkgs.dragon-drop}/bin/dragon --and-exit ~/Code/Meisterlabs/test-data/**/*.*";
+          description = "Dragon: Work DnD Test Data";
+        }
+      ];
+    }
+    };
+    }
