73e99aaf21d3726ead5e8267decb986d9464f596
Florian Schroedl <flo.schroedl@gmail.com>
2021-08-05 23:22:03 +0200

    Configure xmobar

--- modules/desktop/xmobar.hs
+++ modules/desktop/xmobar.hs
@@ -1,4 +1,4 @@
-Config { font = "xft:JetBrains Mono:pixelsize=11:antialias=true:hinting=true"
+Config { font = "xft:Iosevka Mono-6:medium,FontAwesome: size=6"
         , borderColor = "black"
         , border = TopB
         , bgColor = "black"
@@ -7,16 +7,13 @@ Config { font = "xft:JetBrains Mono:pixelsize=11:antialias=true:hinting=true"
         , commands = [ Run Weather "SBPA" ["-t","<tempC>°C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 18000
                         , Run Cpu ["-L","3","-H","50","--normal","lightgreen","--high","red"] 10
                         , Run Memory ["-t","Mem: <used>MB"] 10
-                        , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
+                        , Run Date " %a %d.%m  %H:%M" "date" 10
                         , Run StdinReader
                         , Run DiskU [("/", "<used>/<size>")]
-                          ["-L", "20", "-H", "50", "-m", "1", "-p", "3",
-                          "--normal", "lightgreen", "--high", "red" ] 20
-                        , Run Kbd [ ("us(intl)", "<fc=#5ffa8d>INTL</fc>")
-                                  , ("us"      , "<fc=#5ffaea>US</fc>")
+                          ["-L", "20", "-H", "50", "-m", "1", "-p", "3"] 20
                           ]
                         ]
         , sepChar = "%"
         , alignSep = "}{"
-        , template = "%StdinReader% } <fc=#ee9a00>%date%</fc>  { %kbd% | %cpu% | %memory% | Disk: %disku% | SBPA: %SBPA% "
+        , template = "%StdinReader% } { %cpu% | %memory%   : %disku%   %date%   "
         }
