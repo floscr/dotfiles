cd176bebd0159c1910838b7b6c402acf06a9f8ef
Florian Schroedl <flo.schroedl@gmail.com>
2021-08-11 11:39:46 +0200

    Fix glob attaching giant folder to command

--- hosts/thinknix/etc.nix
+++ hosts/thinknix/etc.nix
@@ -8,11 +8,11 @@
     }
     {
       description = "Playlist: scdl";
-      command = "mpv --shuffle --loop-playlist=yes ~/Media/Music/scdl/**";
+      command = "mpv --shuffle --loop-playlist=yes ~/Media/Music/scdl";
     }
     {
       description = "Playlist: Das hätt es früher nicht gegeben";
-      command = "mpv --shuffle --loop-playlist=yes ~/Media/Music/Das haett es frueher nicht gegeben/**";
+      command = ''mpv --shuffle --loop-playlist=yes "~/Media/Music/Das haett es frueher nicht gegeben"'';
     }
   ];
 
