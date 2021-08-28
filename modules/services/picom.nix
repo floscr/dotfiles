12aed22086432c379ae7eded6ed4db6c746c9a07
Florian Schroedl <flo.schroedl@gmail.com>
2021-08-28 22:30:18 +0200

    Comment opacity rules for now

--- modules/services/picom.nix
+++ modules/services/picom.nix
@@ -28,6 +28,11 @@ in
         "!(_XMONAD_TAGS:s *= 'floating')"
       ];
 
+      # opacityRules = [
+      #   "90:class_g = 'Alacritty' && !_NET_WM_STATE@:32a && _XMONAD_TAGS:s *= 'floating'"
+      #   "0:_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
+      # ];
+
       fade = true;
       fadeDelta = 1;
       fadeSteps = [ 1.0e-2 1.2e-2 ];
@@ -41,17 +46,35 @@ in
 
       settings = {
         shadow-radius = 20;
-        shadow-exclude = [ "!(class_g = 'xmobar' || _XMONAD_TAGS:s *= 'floating')" ];
 
-        blur-background-exclude = [
-          "name *= 'slop'"
-          "name = 'cpt_frame_window'"
-          "name = 'as_toolbar'"
-          "name = 'zoom_linux_float_video_window'"
-          "name = 'AnnoInputLinux'"
-        ];
-        blur-kern = "7x7box";
-        blur-strength = 320;
+        # xinerama-shadow-crop = true;
+        # frame-opacity = 1;
+        # inactive-opacity-override = false;
+        # detect-client-opacity = true;
+        # blur-backgroupnd = true;
+        # blur-background-frame = true;
+        # blur-background-fixed = false;
+        # blur-background-exclude = [
+        #   "window_type = 'dock'"
+        #   "window_type = 'desktop'"
+        # ];
+        # mark-wmwin-focused = true;
+        # mark-ovredir-focused = true;
+        # use-ewmh-active-win = true;
+        # detect-rounded-corners = true;
+        # dbe = false;
+        # unredir-if-possible = false;
+        # detect-transient = true;
+        # detect-client-leader = true;
+        # xrender-sync-fence = true;
+        # blur = {
+        #   method = "kawase";
+        #   strength = 1;
+        #   background = true;
+        #   background-frame = true;
+        #   background-fixed = true;
+        #   kern = "3x3box";
+        # };
 
       };
     };
