b3b551dd6913343c1e8c5c5f147d2d76f79beaf3
Florian Schroedl <flo.schroedl@gmail.com>
2021-10-14 22:10:35 +0200

    Add clojure dev module

---
+++ modules/dev/clojure.nix
@@ -0,0 +1,20 @@
+{ config, options, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.clojure;
+in
+{
+  options.modules.dev.clojure = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      clojure
+      clojure-lsp
+      joker
+      leiningen
+    ];
+  };
+}
