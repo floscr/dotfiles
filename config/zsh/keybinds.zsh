80049156385c92fa3ae677db8ba2ce1c3260bc74
Florian Schroedl <flo.schroedl@gmail.com>
2021-11-03 20:52:17 +0100

    Add Ctrl-T binding for pazi

--- config/zsh/keybinds.zsh
+++ config/zsh/keybinds.zsh
@@ -44,6 +44,7 @@ fi
 autoload -Uz edit-command-line; zle -N edit-command-line
 bindkey '^ ' edit-command-line
 
+bindkey -s "^T" 'pazi view | cut -f 2- | fzf --tiebreak=index | xargs -r bash -c "cd $0"^M'
 bindkey -M viins '^n' history-substring-search-down
 bindkey -M viins '^p' history-substring-search-up
 bindkey -M viins '^s' history-incremental-pattern-search-backward
