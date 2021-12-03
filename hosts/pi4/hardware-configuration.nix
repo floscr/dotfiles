1b2b5f8597dbb6a0a0d32bf71b1597ef269b7ddb
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Use hardware input

--- hosts/pi4/hardware-configuration.nix
+++ hosts/pi4/hardware-configuration.nix
@@ -2,10 +2,10 @@
 
 {
   imports = [
-    "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz"}/raspberry-pi/4"
-  ];
+    inputs.nixos-hardware.nixosModules.raspberry-pi.4
+    ];
 
-  fileSystems = {
+    fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
@@ -13,13 +13,13 @@
     };
   };
 
-  # Enable GPU acceleration
-  boot.loader.raspberryPi.firmwareConfig = ''
+    # Enable GPU acceleration
+    boot.loader.raspberryPi.firmwareConfig = ''
     gpu_mem=320
   '';
-  hardware.raspberry-pi."4".fkms-3d = {
+    hardware.raspberry-pi."4".fkms-3d = {
     enable = true;
     cma = 512;
   };
 
-}
+    }
