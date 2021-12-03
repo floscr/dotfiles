c107277577611048478acd3b72bf6d1bafca7d99
Florian Schroedl <flo.schroedl@gmail.com>
2021-12-03 17:00:00 +0100

    Add useUserPackages

--- modules/options.nix
+++ modules/options.nix
@@ -44,7 +44,16 @@ with lib.my;
     };
 
     home-manager = {
-      # Add aliases
+      useUserPackages = true;
+
+      # I only need a subset of home-manager's capabilities. That is, access to
+      # its home.file, home.xdg.configFile and home.xdg.dataFile so I can deploy
+      # files easily to my $HOME, but 'home-manager.users.hlissner.home.file.*'
+      # is much too long and harder to maintain, so I've made aliases in:
+      #
+      #   home.file        ->  home-manager.users.hlissner.home.file
+      #   home.configFile  ->  home-manager.users.hlissner.home.xdg.configFile
+      #   home.dataFile    ->  home-manager.users.hlissner.home.xdg.dataFile
       users.${config.user.name} = {
         home = {
           file = mkAliasDefinitions options.home.file;
