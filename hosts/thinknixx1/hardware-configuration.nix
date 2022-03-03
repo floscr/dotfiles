d75960171be9ce373ee03fad5cfbdb7c04fd6e76
Florian Schroedl <flo.schroedl@gmail.com>
2022-03-03 09:12:55 +0100

    Add Thinkpad X1 device

---
+++ hosts/thinknixx1/hardware-configuration.nix
@@ -0,0 +1,62 @@
+{ config, lib, pkgs, modulesPath, inputs, ... }:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    # inputs.nixos-hardware.nixosModules.common-cpu-intel
+    # inputs.nixos-hardware.nixosModules.common-pc-laptop-acpi_call
+  ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-intel" ];
+
+  # Remove screen tearing
+  environment.etc."X11/xorg.conf.d/20-intel.conf" = {
+    text = ''
+      Section "Device"
+        Identifier "Intel Graphics"
+        Driver "intel"
+        Option "TearFree" "true"
+        Option "AccelMethod" "sna"
+        Option "SwapbuffersWait" "true"
+        Option "TripleBuffer" "true"
+        Option "VariableRefresh" "true"
+        Option "DRI" "2"
+      EndSection
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    fwupd
+    undervolt
+  ];
+  services.fwupd.enable = true; # Bios updates
+
+  # CPU
+  nix.settings.max-jobs = lib.mkDefault 4;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Harddrives
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/nvme0n1p2";
+      preLVM = true;
+    };
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/55904fe9-fcc7-49e8-8839-8505347d9084";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/FF89-C091";
+    fsType = "vfat";
+  };
+
+  swapDevices = [{
+    device = "/dev/disk/by-uuid/472dca10-b608-4cbc-83f1-2f8980b8a620";
+  }];
+}
---
+++ hosts/thinknixx1/sound.nix
@@ -0,0 +1,178 @@
+{ pkgs, ... }:
+
+{
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      Headset = {
+        FastConnectable = "true";
+        HFP = "true";
+      };
+      General = {
+        ControllerMode = "bredr";
+        Disable = "Headset";
+        AutoEnable = "true";
+        AutoConnect = "true";
+        MultiProfile = "multiple";
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+
+  hardware.pulseaudio = {
+    enable = true;
+    support32Bit = true;
+    package = pkgs.pulseaudioFull;
+    extraModules = [
+      pkgs.pulseaudio-modules-bt
+    ];
+    extraConfig = ''
+      load-module module-udev-detect tsched=0
+      load-module module-bluetooth-policy
+      load-module module-bluetooth-discover
+      load-module module-switch-on-connect
+
+      # Internal Microphone
+      load-module alsa-source device=hw:0,0
+      .ifexists module-udev-detect.so
+    '';
+    # https://medium.com/@gamunu/enable-high-quality-audio-on-linux-6f16f3fe7e1f
+    daemon.config = {
+      default-sample-format = "float32le";
+      default-sample-rate = 48000;
+      alternate-sample-rate = 44100;
+      default-sample-channels = 2;
+      default-channel-map = "front-left,front-right";
+      default-fragments = 2;
+      default-fragment-size-msec = 125;
+      resample-method = "soxr-vhq";
+      remixing-consume-lfe = "no";
+      high-priority = "yes";
+      nice-level = -11;
+      realtime-scheduling = "yes";
+      realtime-priority = 9;
+      rlimit-rtprio = 9;
+      daemonize = "no";
+    };
+  };
+
+  sound.extraConfig = ''
+    # Use PulseAudio plugin hw
+    pcm.!default {
+      type plug
+      slave.pcm hw
+    }
+  '';
+
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      bluez = pkgs.bluez5;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    playerctl
+    pavucontrol
+    blueman
+    (pkgs.writeScriptBin "switch-sound-monitor" ''
+      #!${stdenv.shell}
+
+      # Accept Input + Output from internal hardware
+      pacmd set-card-profile 0 output:hdmi-stereo-extra1
+    '')
+    (pkgs.writeScriptBin "prepare-video-call" ''
+      #!${stdenv.shell}
+
+      # Accept Input + Output from internal hardware
+      pacmd set-card-profile 0 output:analog-stereo+input:analog-stereo
+
+      # Unmute the microphone
+      pactl set-source-mute 21 1
+    '')
+  ];
+
+  modules.bindings.items = [
+    {
+      description = "PulseAudio Volume Control";
+      categories = "Volume Control";
+      command = "pavucontrol";
+    }
+    {
+      description = "Connect Swiss Box";
+      categories = "Script";
+      command = "bluetoothctl connect 30:23:80:C0:7D:85";
+    }
+    {
+      description = "Connect Bluetooth Headphones";
+      categories = "Script";
+      command = "bose_connect";
+    }
+    {
+      description = "Prepare video call";
+      categories = "Script";
+      command = "prepare-video-call";
+    }
+    {
+      description = "Play Monitor Sound";
+      categories = "Script";
+      command = "switch-sound-monitor";
+    }
+    {
+      description = "Toggle Headphone Audio Output";
+      categories = "Script, Audio";
+      command = "nimx toggleHeadphoneAudioOutput";
+    }
+    {
+      description = "Player: Metadata";
+      categories = "Script, Audio";
+      command = "notify-send \"$(playerctl metadata)\"";
+    }
+    {
+      binding = "{ XF86AudioPlay, super + alt + p }";
+      xmonadBinding = "<XF86AudioPlay>";
+      description = "Media: Play / Pause";
+      command = "playerctl play-pause";
+    }
+    {
+      xmonadBinding = "M-M1-p";
+      command = "playerctl play-pause";
+    }
+    {
+      binding = "{ XF86AudioNext, super + alt + l }";
+      xmonadBinding = "<XF86AudioNext>";
+      description = "Media: Next";
+      command = "playerctl next";
+    }
+    {
+      xmonadBinding = "M-M1-l";
+      command = "playerctl next";
+    }
+    {
+      binding = "{ XF86AudioPrev, super + alt + h }";
+      xmonadBinding = "<XF86AudioPrev>";
+      description = "Media: Previous";
+      command = "playerctl previous";
+    }
+    {
+      xmonadBinding = "M-M1-h";
+      command = "playerctl previous";
+    }
+    {
+      binding = "{ shift + XF86AudioNext, super + alt + shift + l, alt + XF86AudioRaiseVolume  }";
+      description = "Media: Forward 20s";
+      command = "playerctl position 20+";
+    }
+    {
+      binding = "{ shift + XF86AudioPrev, super + alt + shift + h, alt + XF86AudioLowerVolume   }";
+      description = "Media: Forward 20s";
+      command = "playerctl position 20-";
+    }
+    {
+      binding = "{ XF86AudioMute, super + alt + m }";
+      xmonadBinding = "<XF86AudioMute>";
+      description = "Sound: Mute/Unmute";
+      command = "pactl set-sink-mute @DEFAULT_SINK@ toggle";
+    }
+  ];
+}
