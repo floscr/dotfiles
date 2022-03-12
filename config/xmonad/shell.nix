d76438d97360b87d9579dd290d41e15f46519155
Florian Schroedl <flo.schroedl@gmail.com>
2022-03-12 18:44:51 +0100

    Add haskell lsp

--- config/xmonad/shell.nix
+++ config/xmonad/shell.nix
@@ -2,10 +2,13 @@
 let
   ghc = pkgs.haskellPackages.ghcWithPackages (pkgs: [
     pkgs.xmonad
+    pkgs.haskell-language-server
     pkgs.xmonad-extras
     pkgs.xmonad-contrib
   ]);
 in
 pkgs.mkShell {
-  buildInputs = [ ghc ];
+  buildInputs = [
+    ghc
+  ];
 }
