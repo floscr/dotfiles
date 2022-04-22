6223c20df7c8dcac19d88052f24db84b22ef515a
Florian Schroedl <flo.schroedl@gmail.com>
2022-04-22 19:26:35 +0200

    Add webcam module

--- modules/hardware/webcam-logitech-c920.nix
+++ modules/hardware/webcam-logitech-c920.nix
@@ -2,10 +2,10 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.hardware.audio;
+let cfg = config.modules.hardware.webcam.logitech-c920;
 in
 {
-  options.modules.hardware.audio = {
+  options.modules.hardware.webcam.logitech-c920 = {
     enable = mkBoolOpt false;
     device = mkStrOpt "/dev/video4";
   };
@@ -15,23 +15,20 @@ in
       v4l-utils # Webcam utils
     ];
 
-    systemd.user.services.set-webcam = {
-      Unit = {
-        Description = "Set the correct settings for the webcam using V4L.";
-        Documentation = [ "man:v4l2-ctl(1)" ];
-      };
-      Install.WantedBy = [ "graphical-session.target" ];
-      Service = {
+    systemd.user.services.setup-webcam = {
+      description = "Set the correct settings for the webcam using V4L.";
+      documentation = [ "man:v4l2-ctl(1)" ];
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig = {
         ExecStart = ''
           ${pkgs.v4l-utils}/bin/v4l2-ctl \
-            --device=${device} \
+            --device=${cfg.device} \
             --set-ctrl=focus_auto=0 \
             --set-ctrl=focus_absolute=0 \
-            --set-ctrl=white_balance_temperature_auto=0 \
             --set-ctrl=contrast=85 \
             --set-ctrl=saturation=140 \
             --set-ctrl=white_balance_temperature=2850 \
-            --set-ctrl=sharpness=100
+            --set-ctrl=sharpness=200
         '';
       };
     };
