e4993527c167a03ec474ddba449d44b407ed732a
Florian Schroedl <flo.schroedl@gmail.com>
2022-04-23 17:06:24 +0200

    Add toggle script

--- modules/services/secure-mode.nix
+++ modules/services/secure-mode.nix
@@ -23,29 +23,50 @@ in
       enableScripts = lib.concatMapStrings (x: x.onEnable + "\n") xs;
       enableBin = (pkgs.writeScriptBin "secure-mode-enable" ''
         #!${pkgs.stdenv.shell}
+        echo "Secure Mode: Enabled"
         touch ${cfg.tmpFile}
         ${enableScripts}
       '');
+      enableScript = "${enableBin}/bin/secure-mode-enable";
+
       disableScripts = lib.concatMapStrings (x: x.onDisable + "\n") xs;
       disableBin = (pkgs.writeScriptBin "secure-mode-disable" ''
         #!${pkgs.stdenv.shell}
+        echo "Secure Mode: Disabled"
         rm -rf ${cfg.tmpFile}
         ${disableScripts}
       '');
+      disableScript = "${disableBin}/bin/secure-mode-disable";
 
+      toggleBin = (pkgs.writeScriptBin "secure-mode-toggle" ''
+        #!${pkgs.stdenv.shell}
+        FILE=${cfg.tmpFile}
+        if [[ -f "$FILE" ]]; then
+            ${disableScript}
+        else
+            ${enableScript}
+        fi
+        ${disableScripts}
+      '');
+      toggleScript = "${toggleBin}/bin/secure-mode-toggle";
     in
     {
       user.packages = with pkgs; [
         enableBin
         disableBin
+        toggleBin
       ];
       modules.bindings.items = [
         {
-          command = "${enableBin}/bin/secure-mode-enable";
+          command = toggleScript;
+          description = "Secure Mode: Toggle";
+        }
+        {
+          command = enableScript;
           description = "Secure Mode: Enable";
         }
         {
-          command = "${disableBin}/bin/secure-mode-disable";
+          command = disableScript;
           description = "Secure Mode: Disable";
         }
       ];
