d8757ce7d11c045b4ebeaf09d744ba5110fca32e
Florian Schroedl <flo.schroedl@gmail.com>
2022-06-09 17:32:50 +0200

    Remove noisetorch

--- hosts/thinknixx1/default.nix
+++ hosts/thinknixx1/default.nix
@@ -15,11 +15,6 @@ with lib.my;
     ".config/espanso/user/email.yml".source = config.age.secrets.espanso-private-emails.path;
   };
 
-  programs.noisetorch = {
-    enable = true;
-    package = with pkgs.unstable; noisetorch;
-  };
-
   user.packages = with pkgs; [
     unzip
     s-tui
