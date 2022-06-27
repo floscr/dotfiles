f3cd9c99a79e12bb87bbebd1f4cf740522e7feca
Florian Schroedl <flo.schroedl@gmail.com>
2022-06-27 09:36:14 +0200

    Add vscodium

--- hosts/pitch.nix
+++ hosts/pitch.nix
@@ -17,6 +17,44 @@ with lib.my;
     };
   };
 
+  home-manager.users.${config.user.name}.programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      # Clojure
+      betterthantomorrow.calva
+
+      # VSpaceCode
+      bodil.file-browser
+      kahole.magit
+      vscodevim.vim
+      vspacecode.vspacecode
+      vspacecode.whichkey
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "fuzzy-search";
+        publisher = "jacobdufault";
+        version = "0.0.3";
+        sha256 = "sha256-oN1SzXypjpKOTUzPbLCTC+H3I/40LMVdjbW3T5gib0M=";
+      }
+      {
+        name = "fuzzy-search";
+        publisher = "jacobdufault";
+        version = "0.0.3";
+        sha256 = "sha256-oN1SzXypjpKOTUzPbLCTC+H3I/40LMVdjbW3T5gib0M=";
+      }
+      {
+        name = "vscode-parinfer";
+        publisher = "shaunlebron";
+        version = "0.6.0";
+        sha256 = "sha256-AMk7GOU1GbANOzACj2Q+vctSMb5zIVSYz4yE5JxFoCI=";
+      }
+    ];
+    # userSettings = with builtins; fromJSON (readFile ./settings.json);
+    # keybindings = with builtins; fromJSON (readFile ./keybindings.json);
+  };
+
+
   modules.bindings.items = [
     {
       description = "Epiphany (Webkit)";
