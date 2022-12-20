58a9e5aaff59c2360c3a86eb701954fd7c3f1bea
Florian Schroedl <flo.schroedl@gmail.com>
2022-12-20 13:49:46 +0100

    Delete old stuff

--- packages/chromium.nix
+++
@@ -1,7 +0,0 @@
-self: super:
-
-{
-  chromium = super.chromium.override {
-    commandLineArgs = ''--add-flags "--force-device-scale-factor=1.5" "--enable-native-notifications"'';
-  };
-}
--- secrets.nix
+++
@@ -1,9 +0,0 @@
-{ options, config, lib, pkgs, ... }:
-
-with lib;
-with lib.my;
-let cfg = config.modules.services.syncthing;
-in {
-  config = mkIf cfg.enable {
-  };
-}
