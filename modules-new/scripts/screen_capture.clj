a27f41f2834aaf46dddeb6b4feb9fb77dbdd7e8c
Florian Schroedl <flo.schroedl@gmail.com>
2023-01-12 08:55:22 +0100

    Structure

--- modules-new/scripts/screen_capture.clj
+++ modules-new/scripts/screen_capture.clj
@@ -4,10 +4,14 @@
             [babashka.fs :as fs]
             [babashka.process :as bp]))
 
+;; Variables -------------------------------------------------------------------
+
 (def default-dirs {:single "~/Media/Screencapture"})
 
 (def cli-opts {:dir {:alias :d
                      :require true}})
+
+;; Helpers ---------------------------------------------------------------------
 
 (defn filename-date []
   (.format (java.text.SimpleDateFormat. "yyyy-MM-dd-HH-mm:ss") (new java.util.Date)))
@@ -18,7 +22,26 @@
                  (fs/expand-home))]
     (str path)))
 
-(defn capture-single! [{:keys [opts]}]
+(defn notification-countdown
+  "Shell like countdown displaying a notification every `update-every` second for `seconds` duration.
+  Uses 1 second as the default `update-every` arg."
+  ([seconds]
+   (notification-countdown 1 seconds))
+  ([update-every seconds]
+   (let [update-every-ms (* update-every 1000)
+
+         repeater (->> (/ seconds update-every)
+                       (+ 1)
+                       (range 1)
+                       (reverse)
+                       (vec))]
+     (doseq [i repeater]
+       (bp/shell "notify-send -u critical" "-t" update-every-ms (format "Recording in %d" i))
+       (bp/shell "sleep" update-every)))))
+
+;; Commands --------------------------------------------------------------------
+
+(defn capture-static! [{:keys [opts]}]
   (let [ext "png"
         path (or
               (:file opts)
@@ -34,14 +57,18 @@
    "screen-capture\n"
    "single Capture a single screenshot\n"))
 
+;; Main ------------------------------------------------------------------------
+
 (def table
-  [{:cmds ["single"] :args->opts [:file] :fn capture-single!}
+  [{:cmds ["single"] :args->opts [:file] :fn capture-static!}
    {:cmds [] :fn help}])
 
 (defn -main [& args]
   (cli/dispatch table args))
 
 (apply -main *command-line-args*)
+
+;; Testing ---------------------------------------------------------------------
 
 (comment
   (-main "single")
