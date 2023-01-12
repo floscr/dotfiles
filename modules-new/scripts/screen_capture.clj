0c6248fa29050a51e7aaac56225adcb7f631b844
Florian Schroedl <flo.schroedl@gmail.com>
2023-01-12 09:47:58 +0100

    Add animated script

--- modules-new/scripts/screen_capture.clj
+++ modules-new/scripts/screen_capture.clj
@@ -2,11 +2,13 @@
 (ns screen-capture
   (:require [babashka.cli :as cli]
             [babashka.fs :as fs]
-            [babashka.process :as bp]))
+            [babashka.process :as bp]
+            [clojure.string :as str]))
 
 ;; Variables -------------------------------------------------------------------
 
-(def default-dirs {:single "~/Media/Screencapture"})
+(def default-dirs {:static "~/Media/Screencapture"
+                   :animated "~/Media/Screenrecording"})
 
 (def cli-opts {:dir {:alias :d
                      :require true}})
@@ -22,11 +24,11 @@
                  (fs/expand-home))]
     (str path)))
 
-(defn notification-countdown
+(defn notification-countdown!
   "Shell like countdown displaying a notification every `update-every` second for `seconds` duration.
   Uses 1 second as the default `update-every` arg."
   ([seconds]
-   (notification-countdown 1 seconds))
+   (notification-countdown! 1 seconds))
   ([update-every seconds]
    (let [update-every-ms (* update-every 1000)
 
@@ -39,17 +41,59 @@
        (bp/shell "notify-send -u critical" "-t" update-every-ms (format "Recording in %d" i))
        (bp/shell "sleep" update-every)))))
 
+(defn parse-int [str]
+  (Integer/parseInt str))
+
+(defn keep-> [x pred]
+  (when (pred x) x))
+
+(defn keep-> [pred x]
+  (when (pred x) x))
+
+(defn discard-> [x pred]
+  (when-not (pred x) x))
+
+(defn discard->> [pred x]
+  (when-not (pred x) x))
+
+(defn get-x-rect []
+  (when-let [[width height x y]
+             (some-> (bp/shell {:out :string
+                                :continue true} "slop -nof '%w %h %x %y'")
+                    :out
+                    (discard-> str/blank?)
+                    (str/split #" ")
+                    (#(map parse-int %)))]
+    {:width width
+     :height height
+     :x x
+     :y y}))
+
 ;; Commands --------------------------------------------------------------------
 
 (defn capture-static! [{:keys [opts]}]
   (let [ext "png"
-        path (or
-              (:file opts)
-              (filename (:single default-dirs) ext))]
+        path (or (:file opts)
+                 (filename (:static default-dirs) ext))]
     (bp/shell {:out path} "maim --delay=0.1 --hidecursor --select --quiet")
     (bp/shell "notify-send" "Screenshot saved" (format "Copied to clipboard\n%s" path))
     (bp/shell "xclip -selection clipboard -t" (format "image/%s" ext) path)
     path))
+
+(defn capture-animated! [{:keys [opts]}]
+  (let [ext "mp4"
+        path (or (:file opts)
+                 (filename (:animated default-dirs) ext))
+        {:keys [width height x y]} (get-x-rect)]
+    (when width
+      (notification-countdown! 3)
+      (bp/shell "ffmpeg" "-y"
+                "-f" "x11grab"
+                "-show_region" "1"
+                "-s" (format "%dx%d" width height)
+                "-i" (format ":0.0+%d,%d" x y)
+                "-framerate" "30"
+                path))))
 
 (defn help
   [_]
@@ -74,6 +118,10 @@
   (-main "single")
   ;; {:cmds (single), :args (), :rest-cmds (), :opts {}, :dispatch [single]}
   (-main "single" "/tmp/example.png")
+
+  (capture-animated! {})
+
+
   ;; {:cmds (single sdfsdf), :args (), :rest-cmds (), :opts {:file sdfsdf}, :dispatch [single]}
   (cli/parse-)
   nil)
