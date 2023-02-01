31d05a71520d35ebdffc638fc73a17aa9521be73
Florian Schroedl <flo.schroedl@gmail.com>
2023-02-01 11:17:26 +0100

    Fix breaking agenix.defaultPackage -> agenix

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -4,20 +4,24 @@ with builtins;
 with lib;
 with lib.my;
 let inherit (inputs) agenix;
-    secretsDir = "${toString ../hosts}/${config.networking.hostName}/secrets";
-    secretsFile = "${secretsDir}/secrets.nix";
-in {
+  secretsDir = "${toString ../hosts}/${config.networking.hostName}/secrets";
+  secretsFile = "${secretsDir}/secrets.nix";
+in
+{
   imports = [ agenix.nixosModules.age ];
-  environment.systemPackages = [ agenix.defaultPackage.x86_64-linux ];
+  environment.systemPackages = [ agenix ];
 
   age = {
     secrets =
       if pathExists secretsFile
-      then mapAttrs' (n: _: nameValuePair (removeSuffix ".age" n) {
-        file = "${secretsDir}/${n}";
-        owner = mkDefault config.user.name;
-      }) (import secretsFile)
-      else {};
+      then
+        mapAttrs'
+          (n: _: nameValuePair (removeSuffix ".age" n) {
+            file = "${secretsDir}/${n}";
+            owner = mkDefault config.user.name;
+          })
+          (import secretsFile)
+      else { };
     identityPaths =
       options.age.identityPaths.default ++ [
         "${config.user.home}/.ssh/dotfiles"
