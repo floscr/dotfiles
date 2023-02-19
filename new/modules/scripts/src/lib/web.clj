93f0057aa6d80dd430025974066db247974bc23b
Florian Schroedl <flo.schroedl@gmail.com>
2023-02-19 22:12:41 +0100

    Add web lib

---
+++ new/modules/scripts/src/lib/web.clj
@@ -0,0 +1,27 @@
+(ns lib.web
+  (:require [clojure.string :as str]))
+
+(def ^:private uri-escape-table
+  {\space "%20"
+   \! "%21"
+   \# "%23"
+   \$ "%24"
+   \% "%25"
+   \& "%26"
+   \' "%27"
+   \( "%28"
+   \) "%29"
+   \* "%2A"
+   \+ "%2B"
+   \, "%2C"
+   \/ "%2F"
+   \: "%3A"
+   \; "%3B"
+   \= "%3D"
+   \? "%3F"
+   \@ "%40"
+   \[ "%5B"
+   \] "%5D"})
+
+(defn uri-escape [uri]
+  (str/escape uri uri-escape-table))
