35a7caaec84c936cb250653a624942220fe190ef
Florian Schroedl <flo.schroedl@gmail.com>
2023-04-13 12:22:17 +0200

    Cleanup

--- modules/services/wireguard.nix
+++ modules/services/wireguard.nix
@@ -23,13 +23,6 @@ in {
         address = [ "10.64.173.1/32" "fc00:bbbb:bbbb:bb01::1:ad00/128" ];
         dns = [ "10.64.0.1" ];
         privateKeyFile = config.age.secrets.mullvad.path;
-        # postUp = ''
-        #   ip route add 192.168.1.0/24 via 192.168.5.1
-        # '';
-        # preDown = ''
-        #   ip route delete 192.168.1.0/24
-        # '';
-
         peers = [
           {
             publicKey = "TNrdH73p6h2EfeXxUiLOCOWHcjmjoslLxZptZpIPQXU=";
