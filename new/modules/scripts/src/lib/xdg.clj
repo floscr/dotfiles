a1cfbccad9f5dd942d21442f839b90879cabc3ec
Florian Schroedl <flo.schroedl@gmail.com>
2023-04-14 16:41:57 +0200

    Add xdg lib file

---
+++ new/modules/scripts/src/lib/xdg.clj
@@ -0,0 +1,18 @@
+(ns lib.xdg
+  (:require
+   [babashka.fs :as fs]))
+
+(defn path [xdg-var args]
+  (when-let [xdg-path (System/getenv xdg-var)]
+    (apply fs/path xdg-path args)))
+
+(defn cache-path [& args]
+  (path "XDG_CACHE_HOME" args))
+
+(defn config-path [& args]
+  (path "XDG_CONFIG_HOME" args))
+
+(comment
+  (cache-path)
+  (config-path)
+  nil)
