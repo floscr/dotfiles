5da3a7473ed177e7e3f889277b8667e79b8a997a
Florian Schroedl <flo.schroedl@gmail.com>
2023-05-05 21:21:40 +0200

    Add monad utils

---
+++ new/modules/scripts/src/lib/monad.clj
@@ -0,0 +1,31 @@
+(ns lib.monad
+  (:require
+   [cats.core :as m]
+   [cats.monad.exception :as exc]))
+
+(defn some-try
+  "Convert a nillable `v` into an `exception` monad with `err` as the failure message."
+  [v err]
+  (if v
+    (exc/success v)
+    (exc/failure (Exception. err))))
+
+(defn tap
+  "Execute a side-effect `f` with the functors value.
+  When `f` fails return the error wrapped in failure.
+  When `f` succeeds, carry on and return functor `fv`."
+  [f fv]
+  (m/bind
+   fv
+   (fn [v]
+     (let [fx (exc/try-on (f v))]
+       (if (exc/success? fx)
+         (m/return v)
+         fx)))))
+
+(comment
+  (->> (exc/success 1)
+       (tap (fn [_] (throw (Exception.)))))
+  (->> (exc/success 1)
+       (tap prn))
+  nil)
