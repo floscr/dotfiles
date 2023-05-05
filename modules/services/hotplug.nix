0d877837cf9b101b8607520243f14443ab9cae9a
Florian Schroedl <flo.schroedl@gmail.com>
2023-05-05 13:45:25 +0200

    Indent

--- modules/services/hotplug.nix
+++ modules/services/hotplug.nix
@@ -44,51 +44,56 @@ in
           Type = "oneshot";
           RemainAfterExit = true;
           ExecStart = "${pkgs.bash}/bin/bash ${pkgs.writeScript "hotplug-monitor.sh" ''
-          #!${pkgs.stdenv.shell}
+            #!${pkgs.stdenv.shell}
 
-          function connectLG(){
-            xrandr \
-              --output eDP-1 --off \
-              --output DP-3 \
-              --primary \
-              --dpi 110 \
-              --panning 3840x2160 \
-              --mode 3840x2160 \
-              --pos 0x0 \
-              --rotate normal \
-              --auto
-          }
+            function connectLG(){
+              xrandr \
+                --output eDP-1 --off \
+                --output DP-3 \
+                --primary \
+                --dpi 110 \
+                --panning 3840x2160 \
+                --mode 3840x2160 \
+                --pos 0x0 \
+                --rotate normal \
+                --auto
+            }
 
-          function disconnect(){
-            xrandr \
-              --output VIRTUAL1 --off \
-              --output DP-1 --off \
-              --output DP-3 --off \
-              --output DP-1 --off \
-              --output HDMI1 --off \
-              --output HDMI2 --off \
-              --output eDP-1 \
-              --primary \
-              --dpi 92 \
-              --auto
-          }
+            function disconnect(){
+              xrandr \
+                --output VIRTUAL1 --off \
+                --output DP-1 --off \
+                --output DP-3 --off \
+                --output DP-1 --off \
+                --output HDMI1 --off \
+                --output HDMI2 --off \
+                --output eDP-1 \
+                --primary \
+                --dpi 92 \
+                --auto
+            }
 
-          if [[ $(xrandr | grep "^DP-3 connected") ]]; then
-            connectLG
-          else
-            disconnect
-          fi
+            setup() {
+                xsetroot -cursor_name left_ptr
 
-          xsetroot -cursor_name left_ptr
+                # emacsclient -e "(my::ui|adjust-ui-to-display)"
 
-          # emacsclient -e "(my::ui|adjust-ui-to-display)"
+                # Hacky way to call the script, but it works
+                /etc/profiles/per-user/$(whoami)/bin/reloadWallpaper
 
-          # Hacky way to call the script, but it works
-          /etc/profiles/per-user/$(whoami)/bin/reloadWallpaper
+                ${pkgs.xorg.xrdb}/bin/xrdb -merge ~/.config/xtheme/80-dpi
 
-          ${pkgs.xorg.xrdb}/bin/xrdb -merge ~/.config/xtheme/80-dpi
+                /etc/profiles/per-user/floscr/bin/xmonadctl restart
+            }
 
-          /etc/profiles/per-user/floscr/bin/xmonadctl restart
+
+            if [[ $(xrandr | grep "^DP-3 connected") ]]; then
+              connectLG
+              setup
+            else
+              disconnect
+              setup
+            fi
         ''}";
         };
       };
