cf2d8b188ed99ef10b2b54e046dedf410f7c9663
Florian Schroedl <flo.schroedl@gmail.com>
2023-05-05 20:33:25 +0200

    Add git root finding functions

--- new/modules/scripts/src/lib/fs.clj
+++ new/modules/scripts/src/lib/fs.clj
@@ -1,6 +1,29 @@
 (ns lib.fs
   (:require [babashka.fs :as fs]))
 
+(defn find-parent-with
+  "Traverse updward from a `path` until `pred` function returns true."
+  [pred path]
+  (cond
+    (pred path) path
+    (fs/parent path) (find-parent-with pred (fs/parent path))
+    :else nil))
+
+(defn find-git-root
+  "Find git root directory for `path`.
+  Returns nil when not found.
+  Ignores worktree roots."
+  [path]
+  (find-parent-with #(fs/directory? (fs/path % ".git")) path))
+
+(defn find-git-worktree-root
+  "Find either the git worktree root or the git root directory for `path`.
+  Returns nil when not found."
+  [path]
+  (find-parent-with #(or (fs/regular-file? (fs/path % ".git"))
+                         (fs/directory? (fs/path % ".git")))
+                    path))
+
 (defn expand
   "Wrapper around `babashka.fs/expand-home`.
   Directly returns the string."
