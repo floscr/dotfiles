dbe7b94e15983cddaad3559235925f3f18e995f3
Florian Schroedl <flo.schroedl@gmail.com>
2023-09-13 13:14:55 +0200

    Add x11 window-info function

---
+++ new/modules/scripts/src/lib/x11.clj
@@ -0,0 +1,37 @@
+(ns lib.x11
+  (:require
+   [babashka.process :as bp]
+   [clojure.string :as str]))
+
+(defn match-window-info-line [line]
+  (let [[_ k _ v] (re-find #"(.+?)(:?\s+)(.*)" line)
+        key (-> k
+                (str/lower-case)
+                (keyword))]
+    {key v}))
+
+(comment
+  (match-window-info-line "Corners:  +0+0  -0+0  -0-0  +0-0")
+  (match-window-info-line "-geometry 3840x2160+0+0")
+  nil)
+
+(defn window-info
+  ([] (window-info "-root"))
+  ([id]
+   (let [info-map
+         (->> (bp/sh ["xwininfo" id])
+              :out
+              (str/split-lines)
+              (eduction
+               (filter #(str/starts-with? % " "))
+               (map str/trim)
+               (map match-window-info-line))
+              (into {}))]
+     (-> info-map
+         (update :width #(Integer/parseInt %))
+         (update :height #(Integer/parseInt %))
+         (update :depth #(Integer/parseInt %))))))
+
+(comment
+  (window-info)
+  nil)
