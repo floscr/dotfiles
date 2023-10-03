eaa1397e4f348274c51c8bb9c4a125f894afccb7
Florian Schroedl <flo.schroedl@gmail.com>
2023-10-03 13:28:30 +0200

    Add xmonad bindings to set monitor brightness

--- new/modules/scripts/monitor_brightness.nix
+++ new/modules/scripts/monitor_brightness.nix
@@ -42,6 +42,46 @@ in
           command = "${cmd} max";
           description = "Max Monitor Brightness (External)";
         }
+        {
+          xmonadBinding = "M-o 1";
+          command = "${cmd} set 10";
+        }
+        {
+          xmonadBinding = "M-o 2";
+          command = "${cmd} set 20";
+        }
+        {
+          xmonadBinding = "M-o 3";
+          command = "${cmd} set 30";
+        }
+        {
+          xmonadBinding = "M-o 4";
+          command = "${cmd} set 40";
+        }
+        {
+          xmonadBinding = "M-o 5";
+          command = "${cmd} set 50";
+        }
+        {
+          xmonadBinding = "M-o 6";
+          command = "${cmd} set 60";
+        }
+        {
+          xmonadBinding = "M-o 7";
+          command = "${cmd} set 70";
+        }
+        {
+          xmonadBinding = "M-o 8";
+          command = "${cmd} set 80";
+        }
+        {
+          xmonadBinding = "M-o 9";
+          command = "${cmd} set 90";
+        }
+        {
+          xmonadBinding = "M-o 0";
+          command = "${cmd} set 100";
+        }
       ];
     };
 }
