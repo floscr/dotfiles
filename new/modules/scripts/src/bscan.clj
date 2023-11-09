0bb3c356b6e8c3e1ee70016e3922228905d6b95f
Florian Schroedl <flo.schroedl@gmail.com>
2023-11-09 16:12:17 +0100

    Add verbose flag

--- new/modules/scripts/src/bscan.clj
+++ new/modules/scripts/src/bscan.clj
@@ -1,4 +1,4 @@
-(ns org-attach
+(ns bscan
   (:require
    [babashka.fs :as fs]
    [babashka.process :as bp]
@@ -9,23 +9,30 @@
    [lib.shell]
    [lib.web]))
 
-(def device-regex
-  "Regex to find my device from the scanimage cli.
+;; Config ----------------------------------------------------------------------
 
-   The output looks something like this:
-     device `v4l:/dev/video2' is a Noname Integrated Camera: Integrated I virtual device
-     device `v4l:/dev/video0' is a Noname Integrated Camera: Integrated C virtual device
-     device `epsonds:libusb:003:034' is a Epson ES-50 ESC/I-2"
+(def device-regex
+  "Regex to find my device from the scanimage cli."
   #"`(epsonds.+)'")
 
 ;; Helpers ---------------------------------------------------------------------
+
+(defn prn-verbose [{:keys [verbose?]} & msgs]
+  (when verbose?
+    (apply println (concat "i" msgs))))
+
+(defn prn-debug
+  ([msg] (prn-debug msg nil))
+  ([msg x]
+   (prn msg)
+   x))
 
 (defn failure [& {:keys [message] :as opts}]
   (exc/failure
    (merge {:kind :scan/error} opts)
    message))
 
-(defn exc->error-message [exc]
+(defn exc->error-message [exc opts]
   (let [err-map (-> (m/extract exc)
                     (Throwable->map))
         custom-err (get-in err-map [:via 0])
@@ -34,35 +41,47 @@
               :else "Something failed")]
     msg))
 
-(defn exc-print! [exc]
+(defn exc-print! [exc opts]
   (let [msg (if (exc/failure? exc)
-              (exc->error-message exc)
+              (exc->error-message exc opts)
               (m/extract exc))]
     (println "Error:" msg))
   exc)
 
+(defn lookup-device
+  "Look up device identifier via `device-regex` in output from 'scanimage -L' shell command.
+
+  The output looks something like this:
+  device `v4l:/dev/video2' is a Noname Integrated Camera: Integrated I virtual device
+  device `v4l:/dev/video0' is a Noname Integrated Camera: Integrated C virtual device
+  device `epsonds:libusb:003:034' is a Epson ES-50 ESC/I-2"
+  [devices-str]
+  (if-let [device-name (some->> devices-str
+                                (map #(re-find device-regex %))
+                                (filter some?)
+                                (first)
+                                (second))]
+    (exc/success device-name)
+    (failure :kind :error/no-device
+             :message "Couldn't find scanner."
+             :verbose-message devices-str)))
+
 ;; Scanning Functions ----------------------------------------------------------
 
-(defn find-device! []
+(defn find-device! [{:keys [verbose? debug?] :as opts}]
+  (prn-verbose opts "Looking up device...")
   (m/mlet [devices (->> (lib.shell/sh-exc "scanimage -L")
                         (m/fmap #(str/split % #"\n")))
-           device (or (some->> devices
-                               (map #(re-find device-regex %))
-                               (filter some?)
-                               (first)
-                               (second)
-                               (exc/success))
-                      (failure :kind :error/no-device
-                               :message "Couldn't find scanner."
-                               :info devices))]
+           device (lookup-device devices)]
+    (prn-verbose opts "Found device:" device)
     (m/return device)))
 
 (comment
-  (find-device!)
+  (find-device! {:verbose? true})
   nil)
 
 (defn scan [& {:as opts}]
-  (-> (m/mlet [device (find-device!)
+  (-> (m/mlet [device (find-device! opts)
                scan-temp-file (-> (fs/create-temp-file {:prefix "scan-"
                                                         :suffix ".pnm"})
                                   (exc/success))
@@ -78,7 +97,7 @@
   (defonce a (atom nil))
 
   (->> (scan :debug? true)
-       (exc-print!)
+       (exc-print! {})
        (reset! a))
 
   (exc-print! @a)
