d5fc57ef58f582fe80b0ffe87f7546aeb64a1632
Florian Schroedl <flo.schroedl@gmail.com>
2023-11-21 12:53:20 +0100

    Remove old hotplug

--- modules/services/hotplug.nix
+++ modules/services/hotplug.nix
@@ -18,7 +18,6 @@ in
       systemd.user.services."hotplug-monitor@" = {
         enable = true;
         description = "Hotplug Monitor";
-        # wantedBy = [ "multi-user.target" ];
         serviceConfig = {
           Type = "oneshot";
           RemainAfterExit = false;
@@ -26,86 +25,11 @@ in
         };
       };
 
-      systemd.user.services."setup-monitor" = {
-        enable = true;
-        description = "Load my monitor modifications";
-        # after = [ "graphical-session-pre.target" ];
-        # wantedBy = [ "graphical-session-pre.target" ];
-        # partOf = [ "graphical-session-pre.target" ];
-        path = with pkgs; [
-          coreutils
-          gnugrep
-          systemd
-          xorg.xrandr
-          xorg.xsetroot
-          # emacsPgtkGcc
-        ];
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-          ExecStart = "${pkgs.bash}/bin/bash ${pkgs.writeScript "hotplug-monitor.sh" ''
-            #!${pkgs.stdenv.shell}
-
-            function connectLG(){
-              xrandr \
-                --output eDP-1 --off \
-                --output DP-3 \
-                --primary \
-                --dpi 110 \
-                --panning 3840x2160 \
-                --mode 3840x2160 \
-                --pos 0x0 \
-                --rotate normal \
-                --auto
-            }
-
-            function disconnect(){
-              xrandr \
-                --output VIRTUAL1 --off \
-                --output DP-1 --off \
-                --output DP-3 --off \
-                --output DP-1 --off \
-                --output HDMI1 --off \
-                --output HDMI2 --off \
-                --output eDP-1 \
-                --primary \
-                --dpi 92 \
-                --auto
-            }
-
-            setup() {
-                xsetroot -cursor_name left_ptr
-
-                # emacsclient -e "(my::ui|adjust-ui-to-display)"
-
-                # Hacky way to call the script, but it works
-                /etc/profiles/per-user/$(whoami)/bin/reloadWallpaper
-
-                ${pkgs.xorg.xrdb}/bin/xrdb -merge ~/.config/xtheme/80-dpi
-
-                /etc/profiles/per-user/floscr/bin/xmonadctl restart
-            }
-
-            display_count=$(xrandr | grep -w " connected" | grep -v "eDP-1 connected" | wc -l)
-
-            if [[ $(xrandr | grep "^DP-3 connected") ]]; then
-              echo "HotPlug: Connecting LG Display"
-              connectLG
-              setup
-            elif [[ "$display_count" -eq 0 ]]
-              echo "HotPlug: Solo Laptop Display"
-              disconnect
-              setup
-            fi
-        ''}";
-        };
-      };
-
       modules.bindings.items = [
         {
           binding = "F12";
           command = restartHotplugServiceCmd;
-          description = "Hotplug Monitor (Old script)";
+          description = "Hotplug Monitor";
         }
       ];
 
