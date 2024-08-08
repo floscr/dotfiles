afdc706ea03f81a20df99c61b6d09d18d0d43461
Florian Schroedl <flo.schroedl@gmail.com>
2024-08-08 07:38:28 +0200

    Cleanup

--- new/modules/scripts/src/lib/recent.clj
+++ new/modules/scripts/src/lib/recent.clj
@@ -5,19 +5,18 @@
    [lib.xdg]))
 
 (def cache-path
-  (fs/path (lib.xdg/cache-path) "babashka-recent"))
+  (lib.xdg/cache-path "babashka-recent"))
 
 (defn cache-file [file-name]
-  (fs/path cache-path (str file-name ".edn")))
+  (fs/file cache-path (str file-name ".edn")))
 
 (defn read-db! [db-name]
-  (some->
-   (try
-     (-> (cache-file db-name)
-         (str)
-         (slurp)
-         (edn/read-string))
-     (catch Exception _ {}))))
+  (try
+    (-> (cache-file db-name)
+        (str)
+        (slurp)
+        (edn/read-string))
+    (catch Exception _ {})))
 
 (defn write-db! [db db-name]
   (let [db-path (cache-file db-name)]
@@ -31,13 +30,16 @@
       (update k (fnil inc 0))
       (write-db! db-name)))
 
+(defn sort-by-db-most-used [db key-fn coll]
+  (sort-by #(get db (key-fn %) 0) > coll))
+
 (defn sort!
   ([coll db-name]
    (sort! coll db-name identity))
   ([coll db-name key-fn]
    (let [db (read-db! db-name)]
      (if (seq db)
-       (sort-by #(get db (key-fn %) 0) > coll)
+       (sort-by-db-most-used db key-fn coll)
        coll))))
 
 (comment
