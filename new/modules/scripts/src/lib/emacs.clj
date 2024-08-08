a194ae105c6afb19e966e11bb99c64343bafdec1
Florian Schroedl <flo.schroedl@gmail.com>
2024-08-08 08:06:10 +0200

    Add run-cmd!

--- new/modules/scripts/src/lib/emacs.clj
+++ new/modules/scripts/src/lib/emacs.clj
@@ -3,4 +3,17 @@
    [babashka.process :as bp]))
 
 (defn open-file! [file-path & {:keys [window? opts]}]
-  (bp/shell opts ["emacsclient" (if window? "-nw" "-n") file-path]))
+  (bp/shell opts "emacsclient" (if window? "-nw" "-n") file-path))
+
+(defn run-cmd! [cmd & {:keys [opts]}]
+  (bp/shell opts "emacsclient" "-e" cmd))
+
+(comment
+  (require '[clojure.string :as str])
+
+  (open-file! ".")
+  (open-file! "." {:opts {:dir "/tmp"}})
+  (open-file! "." {:window? true})
+
+  (run-cmd! "(message \"foo\")")
+  nil)
