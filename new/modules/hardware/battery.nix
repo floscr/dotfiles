fc75d53384911f79f7ceef276be55bed3c64ad9f
Florian Schroedl <flo.schroedl@gmail.com>
2025-01-02 18:24:50 +0100

    Upgrade battery

--- new/modules/hardware/battery.nix
+++ new/modules/hardware/battery.nix
@@ -14,14 +14,14 @@ in
     services.upower.enable = true;
     services.tlp = {
       enable = true;
-      extraConfig = ''
-        START_CHARGE_THRESH_BAT0=75
-        STOP_CHARGE_THRESH_BAT0=92
-        CPU_SCALING_GOVERNOR_ON_AC=performance
-        CPU_BOOST_ON_AC=1
-        CPU_SCALING_GOVERNOR_ON_BAT=powersave
-        ENERGY_PERF_POLICY_ON_BAT=powersave
-      '';
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 92;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_BOOST_ON_AC = 1;
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        ENERGY_PERF_POLICY_ON_BAT = "powersave";
+      };
     };
     services.acpid = {
       enable = true;
