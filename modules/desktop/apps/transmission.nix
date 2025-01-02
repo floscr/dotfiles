f1597aaecf852f9926753aaff066e8d2bede1c0f
Florian Schroedl <flo.schroedl@gmail.com>
2025-01-02 18:21:33 +0100

    Upgrade

--- modules/desktop/apps/transmission.nix
+++ modules/desktop/apps/transmission.nix
@@ -11,7 +11,7 @@ in
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [
-      transmission-gtk
+      transmission_3-gtk
     ];
     home-manager.users.${config.user.name}.xdg.mimeApps = {
       associations.added = {
