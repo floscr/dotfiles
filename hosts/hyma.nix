145c42b8187a515e1f02896352df4444fe72b153
Florian Schroedl <flo.schroedl@gmail.com>
2025-07-09 10:49:58 +0200

    Remove kubernetes

--- hosts/hyma.nix
+++ hosts/hyma.nix
@@ -3,11 +3,6 @@
 {
   user.packages = with pkgs; [
     obsidian
-    (google-cloud-sdk.withExtraComponents [
-      google-cloud-sdk.components.cloud_sql_proxy
-      google-cloud-sdk.components.gke-gcloud-auth-plugin
-    ])
-    kubectl
     python313Packages.playwright
     figma-linux
     python3
